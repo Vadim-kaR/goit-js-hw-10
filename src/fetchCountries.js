57f7d589a4054a1d6b824a6d321ade3f889dc05c
unknown <90862670+Vadim-kaR@users.noreply.github.com>
2022-05-17 20:01:15 +0300

    after fix

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -6,7 +6,8 @@ export function fetchCountries(name) {
     return fetch(`${URL}${name}?fields=name,capital,population,flags,languages`)
         .then((response) => {
             if (!response.ok) {
-                throw new Error(Notiflix.Notify.failure('Oops, there is no country with that name'));
+                Notiflix.Notify.failure('Oops, there is no country with that name');
+                return;
             }
             return response.json()
         })
--- src/index.js
+++ src/index.js
@@ -1,55 +1,38 @@
-var debounce = require('lodash.debounce');
+import oneCountryTpl from './templates/oneCountry.hbs'
+import someCoutriesTpl from './templates/someCounries.hbs'
 import Notiflix from 'notiflix';
-
 import './css/styles.css';
-import { fetchCountries }  from'./fetchCountries.js'
+import { fetchCountries } from './fetchCountries.js'
+import refs from './refs.js'
 
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
-const refs = {
-    input: document.querySelector('#search-box'),
-    list: document.querySelector('.country-list')
+function wrongCountryName() { 
+    refs.countryList.innerHTML = "";
 }
-
-refs.input.addEventListener('input', debounce(onInputValue, DEBOUNCE_DELAY) );
 
 function onInputValue(e) {
     e.preventDefault();
     if (e.target.value.trim() === '') {
         return;
     }
-    fetchCountries(e.target.value.trim()).then(renderData)
+    fetchCountries(e.target.value.trim()).then(renderData).catch(wrongCountryName)
 }
 
+    refs.input.addEventListener('input', debounce(onInputValue, DEBOUNCE_DELAY));
 
-function renderData(data) { 
+function renderData(data) {
 
     if (data.length > 10) { 
          Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-        return
-     }
-    
-    const markup = data.map(({ name, capital, population, flags, languages }) => {
-
-    if (data.length > 2 || data.length >= 10) { 
-        return `<li class="item">
-        <img style="width:40px; height:30px" src="${flags.svg}">
-        <p>${name.official}</p>
-        </li>`
-            
+        return;
     }
-        return `<li>
-        <div class="item"><img style="width:40px; height:30px" src="${flags.svg}">
-        <h1>${name.official}</h1></div>
-        <p><b>Capital:&nbsp</b>${capital}</p>
-        <p><b>Population</b>:&nbsp${population}</p>
-        <p><b>Languages</b>:&nbsp${(Object.values(languages)).join(", ")}</p>
-        </li>`
-        }).join("");
-
-        refs.list.innerHTML = markup; 
-}
     
+    if (data.length > 2 || data.length >= 10) { 
+        refs.countryList.innerHTML = someCoutriesTpl({ data});
+        return;
+    } 
+     refs.countryList.innerHTML = oneCountryTpl({data}); 
+}
 
-
-  
