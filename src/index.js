ae8525803d0c4b4d7586337fbf1a6a74091d284b
unknown <90862670+Vadim-kaR@users.noreply.github.com>
2022-05-18 11:39:17 +0300

    after second fix

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import oneCountryTpl from './templates/oneCountry.hbs'
-
 import someCountriesTpl from './templates/someCounries.hbs'
 import Notiflix from 'notiflix';
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries.js'
-import refs from './refs.js'
+import { fetchCountries } from './js/fetchCountries.js'
+import refs from './js/refs.js'
 
 var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 
-function wrongCountryName() { 
+function eareseCountryInfo() { 
     refs.countryList.innerHTML = "";
+    refs.countryBox.innerHTML = ""; 
 }
 
 function onInputValue(e) {
     e.preventDefault();
     if (e.target.value.trim() === '') {
+        eareseCountryInfo();
         return;
     }
-    fetchCountries(e.target.value.trim()).then(renderData).catch(wrongCountryName)
+    fetchCountries(e.target.value.trim()).then(renderData).catch(eareseCountryInfo)
 }
 
-    refs.input.addEventListener('input', debounce(onInputValue, DEBOUNCE_DELAY));
+
+refs.input.addEventListener('input', debounce(onInputValue, DEBOUNCE_DELAY));
+
 
 function renderData(data) {
 
@@ -31,9 +34,13 @@ function renderData(data) {
     }
     
     if (data.length > 2 || data.length >= 10) { 
-        refs.countryList.innerHTML = someCountriesTpl({ data});
+        eareseCountryInfo();
+        refs.countryList.innerHTML = someCountriesTpl(data);
         return;
     } 
-     refs.countryList.innerHTML = oneCountryTpl({data}); 
+    eareseCountryInfo();
+    refs.countryBox.innerHTML = oneCountryTpl(data); 
 }
+
+
 
--- src/refs.js
+++
@@ -1,4 +0,0 @@
-export default {
-    input: document.querySelector('#search-box'),
-    countryList: document.querySelector('.country-info')
-}
